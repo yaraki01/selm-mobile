676ec8d00be5f8b57a4b48392912127171bc01b4
Ryan Marques <107232087+yaraki01@users.noreply.github.com>
2025-02-10 18:51:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const img1 = document.getElementById('img1')
+const img2 = document.getElementById('img2')
+const img3 = document.getElementById('img3')
+
+img2.addEventListener('click', function(){
+    
+})
