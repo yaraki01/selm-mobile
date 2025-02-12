79d4fabeb044ac6d4c027f20580c498a144f6808
Ryan Marques <107232087+yaraki01@users.noreply.github.com>
2025-02-12 15:38:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const img1 = document.getElementById('img1')
-const img2 = document.getElementById('img2')
-const img3 = document.getElementById('img3')
+const menu = document.getElementById('menu')
+const aside = document.getElementById('aside')
+const divmenu = document.getElementById('div-menu')
 
-img2.addEventListener('click', function(){
-    
+
+menu.addEventListener('click', function(){
+    if(divmenu.style.height == '90px'){
+        divmenu.style.height = '5000px'
+        aside.style.animation = 'aside-apper 0.7s ease-in-out'
+    }
+    else{
+         aside.style.animation = 'aside-desaper 0.7s ease-in-out'
+         divmenu.style.height = '90px'
+        }   
+})
+const instagram = document.querySelector('.img-instagram')
+const whatsapp = document.querySelector('.img-whatsapp')
+
+instagram.addEventListener('click', function(){
+    window.location.href = 'https://www.instagram.com/vemserselm/'
+})
+whatsapp.addEventListener('click', function(){
+    window.location.href = 'https://api.whatsapp.com/send?l=pt-BR&phone=558335762568'
 })
