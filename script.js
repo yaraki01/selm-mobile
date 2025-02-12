255f59453acd948c87a259850e936f690bf71676
Ryan Marques <107232087+yaraki01@users.noreply.github.com>
2025-02-12 19:49:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
+const selmimg = document.querySelector('.selm-img')
+
+selmimg.addEventListener('click', function(){
+    window.location.href = 'index.html'
+})
+
 const menu = document.getElementById('menu')
 const aside = document.getElementById('aside')
-const divmenu = document.getElementById('div-menu')
-
 
 menu.addEventListener('click', function(){
-    if(divmenu.style.height == '90px'){
-        divmenu.style.height = '5000px'
-        aside.style.animation = 'aside-apper 0.7s ease-in-out'
+    if(aside.style.height == '0px'){
+        aside.style.height = '645px'
+        aside.style.animation = 'aside-move 0.7s ease-in-out'
     }
     else{
-         aside.style.animation = 'aside-desaper 0.7s ease-in-out'
-         divmenu.style.height = '90px'
-        }   
+        aside.style.height = '0px'
+        aside.style.animation = 'none'
+    }
 })
-const instagram = document.querySelector('.img-instagram')
-const whatsapp = document.querySelector('.img-whatsapp')
 
-instagram.addEventListener('click', function(){
-    window.location.href = 'https://www.instagram.com/vemserselm/'
+const h3_aside_1 = document.querySelector('.h3-aside-1')
+const h3_aside_2 = document.querySelector('.h3-aside-2')
+const h3_aside_3 = document.querySelector('.h3-aside-3')
+
+const box_aside_1 = document.querySelector('.box-p-aside-1')
+const box_aside_2 = document.querySelector('.box-p-aside-2')
+const box_aside_3 = document.querySelector('.box-p-aside-3')
+const box_aside_4 = document.querySelector('.box-p-aside-4')
+const box_aside_5 = document.querySelector('.box-p-aside-5')
+const box_aside_6 = document.querySelector('.box-p-aside-6')
+const box_aside_7 = document.querySelector('.box-p-aside-7')
+const box_aside_8 = document.querySelector('.box-p-aside-8')
+const box_aside_9 = document.querySelector('.box-p-aside-9')
+
+h3_aside_1.addEventListener('click', function(){
+    if(box_aside_1.style.display == 'none'){
+        box_aside_1.style.display ='block'
+        box_aside_2.style.display ='block'
+        box_aside_3.style.display ='block'
+    }
+    else{
+        box_aside_1.style.display = 'none'
+        box_aside_2.style.display = 'none'
+        box_aside_3.style.display = 'none'
+    }
 })
-whatsapp.addEventListener('click', function(){
-    window.location.href = 'https://api.whatsapp.com/send?l=pt-BR&phone=558335762568'
+h3_aside_2.addEventListener('click', function(){
+    if(box_aside_4.style.display == 'none'){
+        box_aside_4.style.display ='block'
+        box_aside_5.style.display ='block'
+    }
+    else{
+        box_aside_4.style.display = 'none'
+        box_aside_5.style.display = 'none'
+    }
+})
+h3_aside_3.addEventListener('click', function(){
+    if(box_aside_6.style.display == 'none'){
+        box_aside_6.style.display ='block'
+        box_aside_7.style.display ='block'
+         box_aside_8.style.display ='block'
+          box_aside_9.style.display ='block'
+    }
+    else{
+        box_aside_6.style.display = 'none'
+        box_aside_7.style.display = 'none'
+          box_aside_8.style.display = 'none'
+        box_aside_9.style.display = 'none'
+    }
 })
